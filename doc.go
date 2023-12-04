cee8e94b6b8faec5185f0293aef095dc7d2970b9
Sarahtonein <joemoe1899@gmail.com>
2023-12-04 13:53:03 +1000

    init

---
+++ doc.go
@@ -0,0 +1,5 @@
+// doc.go
+// Package main is a simple example package.
+//
+// This package demonstrates how to use the fmt package to print messages.
+package main
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sarahtonein/Documentation-Test
+
+go 1.21.0
